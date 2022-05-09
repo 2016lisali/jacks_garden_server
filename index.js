fa6c734092b4012e0ba6eb5dd23064d5a3caaead
2016lisali <2016lisali@gmai.com>
2022-05-09 21:04:04 +1000

    add netlify site url to the white list

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({
 }));
 
 // allow cross origin resource sharing and restrict to a white list of Ip addresses
-const allowedOrigins = ['https://jacks-garden-admin-panel.herokuapp.com/', 'http://localhost:3002', 'http://localhost:3001', '172.30.211.54']
+const allowedOrigins = ['https://jacksgardenadmin.netlify.app', 'http://localhost:3002', 'http://localhost:3001', '172.30.211.54']
 const corsOptions = {
   origin: allowedOrigins,
   optionsSuccessStatus: 200
