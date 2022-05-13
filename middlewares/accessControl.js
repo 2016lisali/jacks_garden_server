b54aefe67c018db15ddb72267136cb1227788228
2016lisali <2016lisali@gmai.com>
2022-05-13 12:47:23 +1000

    modified cors_admin

--- middlewares/accessControl.js
+++ middlewares/accessControl.js
@@ -1,12 +1,12 @@
 import cors from 'cors';
 const allowedOrigins_store = ["https://jacksgarden.netlify.app", "https://jacksgardenadmin.netlify.app"]
-const allowedOrigins_admin = ["https://jacksgardenadmin.netlify.app", "http://192.168.1.108"]
+const allowedOrigins_admin = ["https://jacksgardenadmin.netlify.app", "192.168.1.108", "http://localhost:3002"]
 
-export const corsOptionsDelegate_admin = (req, origin, callback) => {
+const corsOptionsDelegate_admin = (req, callback) => {
   // allowedOrigins_admin.indexOf(myIpAddress) !== -1 || 
-  const myIpAddress = req.socket.remoteAddress; // This is where you get the IP address from the request
+  const myIpAddress = req.socket?.remoteAddress; // This is where you get the IP address from the request
   console.log(myIpAddress);
-  if (allowedOrigins_admin.indexOf(origin) !== -1) {
+  if (allowedOrigins_admin.indexOf(req.header("origin")) !== -1 && allowedOrigins_admin.indexOf(myIpAddress)) {
     return callback(null, true)
   } else {
     let msg = 'The CORS policy for this site does not allow access from you origin/ip'
@@ -14,15 +14,18 @@ export const corsOptionsDelegate_admin = (req, origin, callback) => {
   }
 }
 
-export const corsOptionsDelegate_store = (origin, callback) => {
-  if (allowedOrigins_store.indexOf(origin) !== -1) {
-    return callback(null, true)
-  } else {
-    let msg = 'The CORS policy for this site does not allow access from you origin/ip'
-    return callback(new Error(msg), false)
-  }
-}
+// const corsOptionsDelegate_store = (origin, callback) => {
+//   if (allowedOrigins_store.indexOf(origin) !== -1) {
+//     return callback(null, true)
+//   } else {
+//     let msg = 'The CORS policy for this site does not allow access from you origin/ip'
+//     return callback(new Error(msg), false)
+//   }
+// }
 
-export const cors_admin = cors({ origin: corsOptionsDelegate_admin })
+export const cors_admin = cors(corsOptionsDelegate_admin)
 
-export const cors_store = cors({ origin: corsOptionsDelegate_store })
+export const cors_store = cors({
+  origin: allowedOrigins_store,
+  optionsSuccessStatus: 200
+})
