e2fa8f6146c35f6fc1a1e04a45da03b2eebc7862
2016lisali <2016lisali@gmai.com>
2022-05-13 12:22:31 +1000

    modified corsOption for admin

--- middlewares/accessControl.js
+++ middlewares/accessControl.js
@@ -1,10 +1,11 @@
 import cors from 'cors';
 const allowedOrigins_store = ["https://jacksgarden.netlify.app", "https://jacksgardenadmin.netlify.app"]
-const allowedOrigins_admin = ["https://jacksgardenadmin.netlify.app", "192.168.1.108"]
+const allowedOrigins_admin = ["https://jacksgardenadmin.netlify.app", "http://192.168.1.108"]
 
 export const corsOptionsDelegate_admin = (req, origin, callback) => {
   const myIpAddress = req.socket.remoteAddress; // This is where you get the IP address from the request
-  if (allowedOrigins_admin.indexOf(myIpAddress) !== -1 && allowedOrigins_admin.indexOf(origin) !== -1) {
+  console.log(myIpAddress);
+  if (allowedOrigins_admin.indexOf(myIpAddress) !== -1 || allowedOrigins_admin.indexOf(origin) !== -1) {
     return callback(null, true)
   } else {
     let msg = 'The CORS policy for this site does not allow access from you origin/ip'
