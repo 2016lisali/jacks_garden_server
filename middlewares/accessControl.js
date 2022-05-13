4e5ac945748f8560ad150f59c1326d6d76537d17
2016lisali <2016lisali@gmai.com>
2022-05-13 14:27:34 +1000

    testing

--- middlewares/accessControl.js
+++ middlewares/accessControl.js
@@ -1,11 +1,10 @@
 import cors from 'cors';
 const allowedOrigins_store = ["https://jacksgarden.netlify.app", "https://jacksgardenadmin.netlify.app"]
-const allowedOrigins_admin = ["https://jacksgardenadmin.netlify.app", "192.168.1.108", "http://localhost:3002"]
+const allowedOrigins_admin = ["https://jacksgardenadmin.netlify.app", "https://jacksgarden.netlify.app", "192.168.1.108", "http://localhost:3002"]
 
 const corsOptionsDelegate_admin = (req, callback) => {
   // allowedOrigins_admin.indexOf(myIpAddress) !== -1 || 
   const myIpAddress = req.socket?.remoteAddress; // This is where you get the IP address from the request
-  console.log(myIpAddress);
   if (allowedOrigins_admin.indexOf(req.header("origin")) !== -1 && allowedOrigins_admin.indexOf(myIpAddress)) {
     return callback(null, true)
   } else {
@@ -23,7 +22,9 @@ const corsOptionsDelegate_admin = (req, callback) => {
 //   }
 // }
 
-export const cors_admin = cors(corsOptionsDelegate_admin)
+export const cors_admin = cors(
+  corsOptionsDelegate_admin
+)
 
 export const cors_store = cors({
   origin: allowedOrigins_store,
