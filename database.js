1aac102ac9d08bd7ce91cffb23d1eb15d1a593e6
2016lisali <2016lisali@gmai.com>
2022-05-16 15:26:02 +1000

    change database to cloud db

--- database.js
+++ database.js
@@ -2,19 +2,19 @@ import mysql from 'mysql2';
 
 // Create a connection to the database
 // CONNECT TO LOCAL DB
-const connection = mysql.createPool({
-  host: "localhost",
-  user: 'root',
-  password: "root",
-  database: "jacks_garden"
-})
-// // CONNECT TO clearDB
 // const connection = mysql.createPool({
-//   host: "us-cdbr-east-05.cleardb.net",
-//   user: "b5c3f2ae918802",
-//   password: "7e172524",
-//   database: "heroku_9961dbe935c2cca"
+//   host: "localhost",
+//   user: 'root',
+//   password: "root",
+//   database: "jacks_garden"
 // })
+// CONNECT TO clearDB
+const connection = mysql.createPool({
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b5c3f2ae918802",
+  password: "7e172524",
+  database: "heroku_9961dbe935c2cca"
+})
 // This wrapper will allow the use of promise functions so that we can use it in any async way along with expressJs
 const query = (sql, parameters) => {
   return new Promise((resolve, reject) => {
